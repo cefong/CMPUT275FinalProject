ed2b014363131640047deed324a679d5492ecf01
cmmartin2514 <58016057+cmmartin2514@users.noreply.github.com>
2020-04-01 18:03:26 -0600

    Implemented moving alien

--- galaga/src/bot.cpp
+++ galaga/src/bot.cpp
@@ -1,24 +1,35 @@
 #include "bot.h"
 static int is_left = 0;
 static alien bot_loc;
-systime_t timestamp_start, timestamp_end;
+systime_t timestamp_start_b, timestamp_end_b, timestamp_start_j, timestamp_end_j;
 void bot() {
     bot_loc.x = WIDTH/2;
     bot_loc.y = 70;
-    timestamp_start = chVTGetSystemTime();
+    int time_delay_bullet = 2000;
+    int time_delay_jump = 5000;
+    timestamp_start_b = chVTGetSystemTime();
+    timestamp_start_j = chVTGetSystemTime();
     while(1) {
         chMsgWait();
         msg_t stat = chMsgGet(engine_thread);
         chMsgRelease(engine_thread, stat);
         if(!stat) {
-            timestamp_end = chVTGetSystemTime();
-            if(timestamp_end - timestamp_start >= TIME_MS2I(2000)) {
-                // fire bullet every 2 seconds
+            timestamp_end_b = chVTGetSystemTime();
+            if(timestamp_end_b - timestamp_start_b >= TIME_MS2I(time_delay_bullet)) {
+                // fire bullet
                 bot_loc.is_fire = true;
-                timestamp_start = chVTGetSystemTime();
+                timestamp_start_b = chVTGetSystemTime();
             }
             else {
+                // don't fire
                 bot_loc.is_fire = false;
+            }
+
+            timestamp_end_j = chVTGetSystemTime();
+            if(timestamp_end_j - timestamp_start_j >= TIME_MS2I(time_delay_jump)) {
+                // move player
+                bot_loc.y += 25;
+                timestamp_start_j = chVTGetSystemTime();
             }
             switch(is_left){
                 case 0:
@@ -29,7 +40,7 @@ void bot() {
                 break;
             }
             
-            chMsgSend(engine_thread, (msg_t)&bot_loc);
+            // constrain x position of bot
             bot_loc.x = constrain(bot_loc.x, bot_size, WIDTH - bot_size);
             if(bot_loc.x <= bot_size && !is_left) {
                 is_left = 1;
@@ -37,6 +48,12 @@ void bot() {
             else if(bot_loc.x >= WIDTH - bot_size && is_left) {
                 is_left = 0;
             }
+            // constrain y position of bot
+            if (bot_loc.y >= HEIGHT - 95) {
+                bot_loc.y = 70;
+            }
+            // send new bot info to engine
+            chMsgSend(engine_thread, (msg_t)&bot_loc);
         }
     }
 }
--- galaga/src/character.cpp
+++ galaga/src/character.cpp
@@ -3,13 +3,20 @@
 static bullet ammo[PLAY_NUM_BULLET];
 
 static void draw_bullet(bool is_player, int x, int y) {
+    // draws a bullet starting from top or bottom depending on character type
     
     if(is_player) {
+        // if player, start from bottom
+        // draw a bullet at current x and y position
         tft.fillRoundRect(x, y, BULLET_WIDTH, BULLET_HEIGHT, BULLET_RAD, BULLET_COLOR_P);
+        // draw black circle at old location
         tft.fillRoundRect(x, y + BULLET_HEIGHT, BULLET_WIDTH, BULLET_HEIGHT, BULLET_RAD, TFT_BLACK);
     }
     else {
+        // if alien, start at top
+        // draw bullet at current x and y pos 
         tft.fillRoundRect(x, y, BULLET_WIDTH, BULLET_HEIGHT, BULLET_RAD, BULLET_COLOR_B);
+        // draw black circle at old location
         tft.fillRoundRect(x, y - BULLET_HEIGHT, BULLET_WIDTH, BULLET_HEIGHT, BULLET_RAD, TFT_BLACK);
     }
 
@@ -18,14 +25,21 @@ static void draw_bullet(bool is_player, int x, int y) {
 
 
 void draw_bot(int x, int y, int x_pos, int y_pos) {
+    // draw alien: xy are new pos, x_pos/y_pos are old
+    // draw black circle at old position
     tft.fillCircle(x_pos, y_pos, bot_size, TFT_BLACK);
+    // draw blue circle at new position
     tft.fillCircle(x, y, bot_size, BOT_COLOR);
 }
 
 void draw_player(int x, int y, int x_pos, int y_pos) {
+    // draw player: xy are new pos, x_pos/y_pos are old
+    // draw black circle at old position
     tft.fillCircle(x_pos, y_pos, player_size, TFT_BLACK);
+    // draw red circle at new position
     tft.fillCircle(x, y, player_size, PLAYER_COLOR);
 }
+
 void fire_bullet(bool is_player, int x, int y) {
     for(int i = 0; i < PLAY_NUM_BULLET; i++) {
         if(!ammo[i].active) {
