2dc01af147bc84de65eafc705d5f5d6f53bb0b15
cmmartin2514 <58016057+cmmartin2514@users.noreply.github.com>
2020-04-07 13:22:35 -0600

    Added comments and cleaned up some code

--- galaga/src/character.cpp
+++ galaga/src/character.cpp
@@ -1,10 +1,24 @@
+/*
+Names: Claire Martin (1571140), Celine Fong, Minh Dang, Zi Xue Lim
+CMPUT 275, Winter 2020
+
+Final Project: A Variation on 'Galaga'
+character.cpp: Character/graphics control
+*/
+
 #include "character.h"
 
 // define array of bullet structs (size is number allowed on screen at once)
 bullet ammo[PLAY_NUM_BULLET];
+
 static void draw_bullet(bool is_player, int x, int y) {
     /*
 	Draws a bullet starting from top or bottom depending on character type.
+
+	PARAMETERS:
+		is_player: True if the character shooting is a player, false if it is an alien
+		x: current x position of bullet
+		y: current y position of bullet
     */
     if(is_player) {
         // if player, start from bottom
@@ -17,7 +31,7 @@ static void draw_bullet(bool is_player, int x, int y) {
         // if alien, start at top
         // draw bullet at current x and y pos 
         tft.fillRoundRect(x, y, BULLET_WIDTH, BULLET_HEIGHT, BULLET_RAD, BULLET_COLOR_B);
-        // draw black circle at old location
+        // draw black rectangle at old location
         tft.fillRoundRect(x, y - BULLET_HEIGHT, BULLET_WIDTH, BULLET_HEIGHT, BULLET_RAD, TFT_BLACK);
     }
 
@@ -26,6 +40,15 @@ static void draw_bullet(bool is_player, int x, int y) {
 
 
 void drawHeart(int16_t anchorX, int16_t anchorY, int16_t scale, int16_t color = TFT_RED) {
+	/*
+	Draws a bullet starting from top or bottom depending on character type.
+
+	PARAMETERS:
+		anchorX: Anchor x position for bitmap (top left)
+		anchorY: Anchor y position for bitmap (top left)
+		scale: Chosen scale of bitmap (able to make large or smaller)
+		color: Chosen colour of character (default red for aliens)
+    */
     // anchor point in upper left corner
     tft.drawRect(anchorX, anchorY+2*scale, scale, 3*scale, color);
     tft.drawRect(anchorX+1*scale, anchorY+1*scale, scale, 5*scale, color);
@@ -39,18 +62,23 @@ void drawHeart(int16_t anchorX, int16_t anchorY, int16_t scale, int16_t color = 
 }
 
 
-void drawSpaceship(player_alien* player, int16_t scale) {
+void drawSpaceship(player_alien* ship, int16_t scale) {
 	/*
 	Draws the spaceships at player and bot locations
+
+	PARAMETERS:
+		ship: The character to be drawn (can be either player or alien)
+		scale: Size of character
 	*/
 
-	if(player->is_active) {
+	if(ship->is_active) {
 		int16_t color1, color2, color3, color4, color5, color6, color7;
-		int16_t anchorX = player->x;
-		int16_t anchorY = player->y;
+		int16_t anchorX = ship->x;
+		int16_t anchorY = ship->y;
 		// change colour of ship based on character type
-		if (player->is_player) {
-			tft.fillCircle(player->x_temp, player->y_temp, size*scale, TFT_BLACK);
+		if (ship->is_player) {
+			// if player, use player colours
+			tft.fillCircle(ship->x_temp, ship->y_temp, size*scale, TFT_BLACK);
 			color1 = COLOR_1_PLAYER;
 			color2 = COLOR_2_PLAYER;
 			color3 = COLOR_3_PLAYER;
@@ -60,7 +88,8 @@ void drawSpaceship(player_alien* player, int16_t scale) {
 			color7 = COLOR_7_PLAYER;
 		} 
 		else {
-			tft.fillCircle(player->x_temp, player->y_temp, (size+1)*scale, TFT_BLACK);
+			// if alien, use alien colours
+			tft.fillCircle(ship->x_temp, ship->y_temp, (size+1)*scale, TFT_BLACK);
 			scale *= -1;
 			color1 = COLOR_1_ENEMY;
 			color2 = COLOR_2_ENEMY;
@@ -70,8 +99,9 @@ void drawSpaceship(player_alien* player, int16_t scale) {
 			color6 = COLOR_6_ENEMY;
 			color7 = COLOR_7_ENEMY;
 		}
-		player->x_temp = player->x;
-		player->y_temp = player->y;
+		// update x and y position
+		ship->x_temp = ship->x;
+		ship->y_temp = ship->y;
 		// code for color 1
 		tft.fillRect(anchorX, anchorY-4*scale, scale, scale, color1);
 		tft.fillRect(anchorX, anchorY+6*scale, scale, scale, color1);
@@ -142,30 +172,29 @@ void drawSpaceship(player_alien* player, int16_t scale) {
 
 }
 
-// void draw_bot(int x, int y, int x_pos, int y_pos) {
-//     tft.fillCircle(x_pos, y_pos, bot_size, TFT_BLACK);
-//     tft.fillCircle(x, y, bot_size, BOT_COLOR);
-// }
 
-// void draw_player(int x, int y, int x_pos, int y_pos) {
-//     tft.fillCircle(x_pos, y_pos, player_size, TFT_BLACK);
-//     tft.fillCircle(x, y, player_size, PLAYER_COLOR);
-// }
-
-void fire_bullet(player_alien *player) {
+void fire_bullet(player_alien *ship) {
 	/*
-	Fire bullet from player or bot
+	Fire bullet from player or bot.
+
+	PARAMETERS:
+		ship: The character that is currently firing
 	*/
-	int x = player->x;
-	int y = player->y;
-	bool is_player = player->is_player;
+
+	// define x and y starting positions of bullet
+	int x = ship->x;
+	int y = ship->y;
+	// determine if bullet should come from bottom or top (alien or player)
+	bool is_player = ship->is_player;
     for(int i = 0; i < PLAY_NUM_BULLET; i++) {
 		// allows us to have multiple bullets going at once
         if(!ammo[i].active) {
             ammo[i].active = true;
             ammo[i].player = is_player;
             ammo[i].x = x;
+			// if player, move up the screen (subtract from y position)
             if(is_player) ammo[i].y = y - 2 * BULLET_HEIGHT;
+			// if alien, move down the screen
             else ammo[i].y = y + 2 * BULLET_HEIGHT;            
             break;
         }
@@ -174,16 +203,37 @@ void fire_bullet(player_alien *player) {
 
 
 void drawExplosion(int x, int y, int radius, uint16_t colorBull){
+	/*
+	Draws an explosion surrounding a chosen character
+
+	PARAMETERS:
+		x: x position of character
+		y: y position of character
+		radius: radius of circle
+		colourBull: colour of explosion (depends on character type)
+    */
     tft.fillCircle(x, y, radius, colorBull);
     tft.fillCircle(x, y, radius, TFT_BLACK);
 }
 
+
 void bullet_update(player_alien *bot, player_alien *player) {
+	/*
+	Updates bullet positions for bots and players.
+
+	PARAMETERS:
+		bot: The alien in play
+		player: The player
+    */
+
+    // iterate through each bullet
     for(int i = 0; i < PLAY_NUM_BULLET; i++) {
         if(ammo[i].active) {
+			// draw active bullets
             draw_bullet(ammo[i].player, ammo[i].x, ammo[i].y);
             if(ammo[i].y < 50 + 2*BULLET_HEIGHT || ammo[i].y > HEIGHT - 50 - 2*BULLET_HEIGHT) {
-                tft.fillRoundRect(ammo[i].x, ammo[i].y, BULLET_WIDTH, BULLET_HEIGHT, BULLET_RAD, TFT_BLACK);
+				// if bullet hits edge of screen, deactivate and remove it
+				tft.fillRoundRect(ammo[i].x, ammo[i].y, BULLET_WIDTH, BULLET_HEIGHT, BULLET_RAD, TFT_BLACK);
                 ammo[i].active = false;
             }
             else {
@@ -192,10 +242,14 @@ void bullet_update(player_alien *bot, player_alien *player) {
                     ammo[i].y -= BULLET_HEIGHT;
                     tft.setTextColor(TFT_RED);
                     if((((bot->x)+15) >= ammo[i].x) && (((bot->x)-15) <= ammo[i].x)  && (((bot->y)+15) >= ammo[i].y) && (((bot->y)-15) <= ammo[i].y) && bot->is_active ){
-                        ammo[i].active=0;
-                        drawExplosion(bot->x,bot->y , 35, TFT_CYAN); // put radius as 40 so dont have to delete bullets 
+                        // if bullet hits the alien
+						ammo[i].active = false;
+						 // radius is 35 so we don't have to delete bullets 
+                        drawExplosion(bot->x,bot->y , 35, TFT_CYAN);
+						// remove a life from the bot
                         bot->lives = (bot->lives)-1;
-                        if((bot -> lives) == 0){ // do something when the bot dies.
+                        if((bot -> lives) == 0){
+							// if bot is dead
 							bot -> is_active = false;
 							bot -> x = 0;
 							bot -> y = 0;
@@ -210,15 +264,20 @@ void bullet_update(player_alien *bot, player_alien *player) {
                 	}
                 }
                 else {
+					// if bullet was fired by alien
                     ammo[i].y += BULLET_HEIGHT;
                     if((((player->x)+15) >= ammo[i].x) && (((player->x)-15) <= ammo[i].x)  && (((player->y)+15) >= ammo[i].y) && (((player->y)-15) <= ammo[i].y) && player->is_active ){
-                        ammo[i].active=0;
-                        drawExplosion(player->x,player->y , 29, TFT_GREEN); // put radius samller than bot cause the floor gets removed at 40
+                        // if bullet hits player
+						ammo[i].active=0;
+						 // radius is only 29 because the floor gets removed at 40
+                        drawExplosion(player->x,player->y , 29, TFT_GREEN);
+						// remove a life
                         player->lives = (player->lives)-1;
 						// erase heart at bottom of screen
 						tft.fillRect(10+(player->lives)*30, HEIGHT-20, 18, 18, TFT_BLACK);
-                        if((player -> lives) == 0){ // do something when the player dies.
-                            drawExplosion(player->x,player->y , 29, TFT_RED); // make sure game over
+                        if((player -> lives) == 0){
+							// if player is dead
+                            drawExplosion(player->x,player->y , 29, TFT_RED);
 							player -> is_active = false;
 							// exit out of game, display score and display option to return to main screen
                         }
